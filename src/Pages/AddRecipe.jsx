a169f5c7cfa81f5677d2e61c41ad4859a72c90ba
Rosemary <Rosemary>
2023-04-12 19:44:40 +0200

    add change in pages

--- src/Pages/AddRecipe.jsx
+++ src/Pages/AddRecipe.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
-import "../Style/Addrecipe.css";
+import "../Style/AddRecipe.css";
 
 function RecipeForm() {
     const [name, setName] = useState("");
     const [ingredients, setIngredients] = useState("");
     const [preparationTime, setPreparationTime] = useState("");
     const [category, setCategory] = useState("");
-   // const [servings, setServings] = useState("");
     const [preparationMethod, setPreparationMethod] = useState("");
     const [allergens, setAllergens] = useState("");
 
@@ -27,10 +26,6 @@ function RecipeForm() {
         setCategory(event.target.value);
     };
 
-    // const handleServingsChange = (event) => {
-    //     setServings(event.target.value);
-    // };
-
     const handlePreparationMethodChange = (event) => {
         setPreparationMethod(event.target.value);
     };
@@ -48,48 +43,39 @@ function RecipeForm() {
         <div className="recipe-form-container">
             <Form onSubmit={handleSubmit}>
                 <Row>
-                    <Col>
+                    <Col md={6}>
                         <Form.Group className="mb-3">
                             <Form.Label>Nombre</Form.Label>
                             <Form.Control type="text" placeholder="Nombre" value={name} onChange={handleNameChange} required />
                         </Form.Group>
-
                         <Form.Group className="mb-3">
                             <Form.Label>Ingredientes</Form.Label>
                             <Form.Control type="text" placeholder="Ingredientes" value={ingredients} onChange={handleIngredientsChange} required />
                         </Form.Group>
-
                         <Form.Group className="mb-3">
                             <Form.Label>Tiempo de preparación</Form.Label>
                             <Form.Control type="text" placeholder="Tiempo de preparación" value={preparationTime} onChange={handlePreparationTimeChange} required />
                         </Form.Group>
-
                         <Form.Group className="mb-3">
                             <Form.Label>Categoría</Form.Label>
                             <Form.Control type="text" placeholder="Categoría" value={category} onChange={handleCategoryChange} required />
                         </Form.Group>
-
                         <p>Servicio para 4 personas</p>
                     </Col>
-        
-                    <Col>
+                    <Col md={6}>
                         <Form.Group className="mb-3">
                             <Form.Label>Modo de preparación</Form.Label>
                             <Form.Control as="textarea" rows={3} placeholder="Modo de preparación" value={preparationMethod} onChange={handlePreparationMethodChange} required />
                         </Form.Group>
-
                         <Form.Group className="mb-3">
                             <Form.Label>Alérgenos</Form.Label>
                             <Form.Control type="text" placeholder="Alérgenos" value={allergens} onChange={handleAllergensChange} />
                         </Form.Group>
-
                         <Row>
                             <Col xs="auto" className="text-end">
                                 <Button variant="primary" type="submit">Añadir receta</Button>
                             </Col>
                         </Row>
-
-
                     </Col>
                 </Row>
             </Form>
@@ -98,6 +84,7 @@ function RecipeForm() {
 }
 
 export default RecipeForm;
+
 
 
 
