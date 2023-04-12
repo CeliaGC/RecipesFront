34f211c7a5844d660a82a23354d9ef31c18bae92
Rosemary <Rosemary>
2023-04-12 11:13:17 +0200

    add file Addrecipe.jsx

--- src/Component/Addrecipe.jsx
+++ src/Component/Addrecipe.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
-//import "../Style/Register.css";
+import "../Style/Addrecipe.css";
 
 function RecipeForm() {
   const [name, setName] = useState("");
