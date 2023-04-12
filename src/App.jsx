8ff7bfe619d045e662df69005f6ee6c158dae06f
Rosemary <Rosemary>
2023-04-12 09:32:35 +0200

    add change in Register.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
          <Root style={{ background: '#F2CC49' }}></Root>
-
+        
       </div>
   )
 }
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,10 +1,11 @@
-
+import Register from "../Component/register"
 
 function Home() {
     return (
       <div className='App'>
       
           <h1>este es el home</h1>
+          <Register />
         </div>
     )
   }
