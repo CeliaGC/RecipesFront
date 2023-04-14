bc5f181db399b7d9581b27a489942fbd802a7536
Rosemary <Rosemary>
2023-04-14 09:59:34 +0200

    add change in home.jsx

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,11 +1,11 @@
-import EditRecipeForm from "../Component/EditRecipe.jsx";
+import Footer from "../src/Component/Footer.jsx";
 
 function Home() {
     return (
       <div className='App'>
       
           <h1>este es el home</h1>
-          <EditRecipeForm />
+          <Footer />
         </div>
     )
 
