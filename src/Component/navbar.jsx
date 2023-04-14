32950643f06bc40ce510a8bda5c885ab70f91acf
acalabustos <acalabustos@gmail.com>
2023-04-14 16:49:16 +0200

    add new pages

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -1,35 +1,45 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import '../Style/Navbar.css'
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
-
-function Navb() {
+const NavbarComponent = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-      {/* <Navbar.Toggle aria-controls="navb" /> */}
-        <Navbar.Collapse id="navb" className='ms-auto'>
-            <Nav.Link href="#" disabled>
+    <Navbar style={{ backgroundColor: "#F2AA85" }} >
+      
+      <Navbar.Brand href="#home">
+        <img
+          src={logo}
+          alt="Logo"
+          width="70"
+          height="70"
+          className="d-inline-block"
+        />
+        <span className="ml-">CookSmart</span>
+      </Navbar.Brand>
+
+     
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+    
+        <Nav>
+          <Nav.Link href="#login">
+            {/* <i className="fa fa-user-circle-o" aria-hidden="true" /> Login */}
             <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
             </svg> 
-            </Nav.Link>
-          <Form className="d-flex col-xs-6 col-md-4 ms-auto">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+          </Nav.Link>
+        </Nav>
+        <Form inline className="d-flex col-xs-6 ">
+          <FormControl
+            type="text"
+            placeholder="Buscar"
+            className="mr-sm-2"
+          />
+          <Button variant="light">Buscar</Button>
+        </Form>
+      </Navbar.Collapse>
     </Navbar>
   );
-}
+};
 
-export default Navb;
+export default NavbarComponent;
