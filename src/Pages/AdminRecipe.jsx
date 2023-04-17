d4853d1ad424a56ca2909ba1431245df2baf8977
Rosemary <Rosemary>
2023-04-17 22:03:59 +0200

    add change in AdminRecipe.jsx

--- src/Pages/AdminRecipe.jsx
+++ src/Pages/AdminRecipe.jsx
@@ -1,37 +1,79 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import AdminRecipe from "../pages/AdminRecipe.jsx";
+import React, { useState } from "react";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
 
-function AdminRecipe() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Form>
-              <Form.Group controlId="recipeName">
-                <Form.Label>Nombre de la receta</Form.Label>
-                <Form.Control type="text" placeholder="Ingrese el nombre de la receta" />
-              </Form.Group>
-  
-              <Form.Group controlId="recipeIngredients">
-                <Form.Label>Ingredientes</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Ingrese los ingredientes separados por coma" />
-              </Form.Group>
-  
-              <Form.Group controlId="recipeInstructions">
-                <Form.Label>Instrucciones</Form.Label>
-                <Form.Control as="textarea" rows={5} placeholder="Ingrese las instrucciones" />
-              </Form.Group>
-  
-              <Button variant="primary" type="submit">
-                Agregar receta
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+const AdminRecipe = () => {
+  const [recipes, setRecipes] = useState([
+    {
+      id: 1,
+      name: "Pasta Alfredo",
+      category: "Pasta",
+      ingredients: ["pasta", "butter", "garlic", "parmesan cheese", "cream"],
+      instructions: "Cook pasta according to package instructions...",
+      imageUrl: "https://example.com/pasta-alfredo.jpg",
+      isPublished: true,
+    },
+    {
+      id: 2,
+      name: "Chicken Tacos",
+      category: "Mexican",
+      ingredients: ["chicken", "tortillas", "avocado", "salsa", "cheese"],
+      instructions: "Grill chicken until cooked through...",
+      imageUrl: "https://example.com/chicken-tacos.jpg",
+      isPublished: false,
+    },
+  ]);
+
+  const handleDeleteRecipe = (id) => {
+    const updatedRecipes = recipes.filter((recipe) => recipe.id !== id);
+    setRecipes(updatedRecipes);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Manage Recipes</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Ingredients</th>
+                <th>Instructions</th>
+                <th>Image</th>
+                <th>Published</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {recipes.map((recipe) => (
+                <tr key={recipe.id}>
+                  <td>{recipe.name}</td>
+                  <td>{recipe.category}</td>
+                  <td>{recipe.ingredients.join(", ")}</td>
+                  <td>{recipe.instructions}</td>
+                  <td>
+                    <img src={recipe.imageUrl} alt={recipe.name} />
+                  </td>
+                  <td>{recipe.isPublished ? "Yes" : "No"}</td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      onClick={() => handleDeleteRecipe(recipe.id)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AdminRecipe;
 
   
