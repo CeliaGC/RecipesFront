d4c06099b830b03695709a00fc5c41c149fb24ce
Rosemary <Rosemary>
2023-04-18 21:19:06 +0200

    add change in AddRecipe

--- src/Component/AddRecipe.jsx
+++ src/Component/AddRecipe.jsx
@@ -42,44 +42,44 @@ function RecipeForm() {
     return (
         <div className="recipe-form-container">
             <Form onSubmit={handleSubmit}>
-                <Row>
-                    <Col md={6}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Nombre</Form.Label>
-                            <Form.Control type="text" placeholder="Nombre" value={name} onChange={handleNameChange} required />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Ingredientes</Form.Label>
-                            <Form.Control type="text" placeholder="Ingredientes" value={ingredients} onChange={handleIngredientsChange} required />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Tiempo de preparación</Form.Label>
-                            <Form.Control type="text" placeholder="Tiempo de preparación" value={preparationTime} onChange={handlePreparationTimeChange} required />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Categoría</Form.Label>
-                            <Form.Control type="text" placeholder="Categoría" value={category} onChange={handleCategoryChange} required />
-                        </Form.Group>
-                        <p>Servicio para 4 personas</p>
-                    </Col>
-                    <Col md={6}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Modo de preparación</Form.Label>
-                            <Form.Control as="textarea" rows={3} placeholder="Modo de preparación" value={preparationMethod} onChange={handlePreparationMethodChange} required />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Alérgenos</Form.Label>
-                            <Form.Control type="text" placeholder="Alérgenos" value={allergens} onChange={handleAllergensChange} />
-                        </Form.Group>
-                        <Row>
-                            <Col xs="auto" className="text-end">
-                                <Button variant="primary" type="submit">Añadir receta</Button>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Form>
-        </div>
+  <Row>
+    <Col md={2}>
+      <Form.Group className="mb-3">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="Nombre" value={name} onChange={handleNameChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Ingredientes</Form.Label>
+        <Form.Control type="text" placeholder="Ingredientes" value={ingredients} onChange={handleIngredientsChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Tiempo de preparación</Form.Label>
+        <Form.Control type="text" placeholder="Tiempo de preparación" value={preparationTime} onChange={handlePreparationTimeChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Categoría</Form.Label>
+        <Form.Control type="text" placeholder="Categoría" value={category} onChange={handleCategoryChange} required />
+      </Form.Group>
+      <p>Servicio para 4 personas</p>
+    </Col>
+    <Col md={2}>
+      <Form.Group className="mb-3">
+        <Form.Label>Modo de preparación</Form.Label>
+        <Form.Control as="textarea" rows={3} placeholder="Modo de preparación" value={preparationMethod} onChange={handlePreparationMethodChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Alérgenos</Form.Label>
+        <Form.Control type="text" placeholder="Alérgenos" value={allergens} onChange={handleAllergensChange} />
+      </Form.Group>
+      <Row>
+        <Col xs="auto" className="text-end">
+          <Button variant="primary" type="submit">Añadir receta</Button>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+</Form>
+</div>
     );
 }
 
