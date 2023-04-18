af3ff32098c411be52bc86fb9877894d51e626d6
Rosemary <Rosemary>
2023-04-18 11:27:30 +0200

    add changes

---
+++ src/Pages/AdminRecipe.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
+import "../Style/AdminRecipe.css";
+
+const AdminRecipe = () => {
+  const [recipes, setRecipes] = useState([
+    { id: 1, name: "Ensalada César", category: "Ensaladas" },
+    { id: 2, name: "Lasaña de Carne", category: "Pastas" },
+    { id: 3, name: "Tarta de Limón", category: "Postres" },
+    { id: 4, name: "Sopa de Calabaza", category: "Sopas" },
+    { id: 5, name: "Hamburguesa con queso", category: "Carnes" },
+    { id: 6, name: "Arroz con mariscos", category: "Arroces" }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage] = useState(3);
+
+  const indexOfLastRecipe = currentPage * recipesPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+  const renderRecipes = currentRecipes.map((recipe) => {
+    return (
+      <Col xs={4} key={recipe.id}>
+        <Card className="mb-4 recipe-card">
+          <Card.Body>
+            <Card.Title>{recipe.name}</Card.Title>
+            <Card.Text>{recipe.category}</Card.Text>
+            <div className="btn-group">
+              <Button variant="warning" size="sm" style={{backgroundColor: "rgba(252, 163, 117, 1)"}}>
+                Editar
+              </Button>
+              <Button variant="danger" size="sm" style={{backgroundColor: "rgba(220, 197, 167, 1)"}}>
+                Eliminar
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  });
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(recipes.length / recipesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const renderPageNumbers = pageNumbers.map((number) => {
+    return (
+      <li key={number} className={currentPage === number ? "page-item active" : "page-item"}>
+        <Button variant="link" className="page-link" onClick={() => setCurrentPage(number)}>
+          {number}
+        </Button>
+      </li>
+    );
+  });
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Recetas</h1>
+          <Form>
+            <Form.Group controlId="formBasicSearch">
+              <Form.Control type="text" placeholder="Buscar por receta" style={{backgroundColor: "rgba(255, 236, 227, 1)"}}>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+          <hr />
+          <Row>{renderRecipes}</Row>
+          <nav>
+          <ul className="pagination justify-content-center" style={{marginBottom: "0.5rem"}}>
+  {renderPageNumbers}
+</ul>
+
+          </nav>
+          <hr />
+          <h3>Añadir receta</h3>
+          <Form>
+            <Form.Group controlId="formBasicReceta">
+              <Form.Label>Receta</Form.Label>
+              <Form.Control type="text" placeholder="Introduce una receta" style={{backgroundColor: "rgba(255, 236, 227, 1)"}}>
+
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit" style={{backgroundColor: "rgba(252, 163, 117, 1)", marginTop: "1rem"}}>
+              Añadir
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AdminRecipe;
---
+++ src/Pages/AdminUser.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
+import "../Style/AdminUser.css";
+
+const AdminUser = () => {
+  // Array de usuarios de ejemplo
+  const initialUsers = [
+    {
+      id: 1,
+      name: "Juan",
+      email: "juan@mail.com",
+      role: "admin",
+    },
+    {
+      id: 2,
+      name: "María",
+      email: "maria@mail.com",
+      role: "user",
+    },
+    {
+      id: 3,
+      name: "Pedro",
+      email: "pedro@mail.com",
+      role: "user",
+    },
+  ];
+  // Estado que almacena la lista de usuarios
+  const [users, setUsers] = useState(initialUsers);
+  // Función para eliminar un usuario de la lista
+  const handleDeleteUser = (id) => {
+    const updatedUsers = users.filter((user) => user.id !== id);
+    setUsers(updatedUsers);
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Administrar usuarios</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Rol</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      onClick={() => handleDeleteUser(user.id)}
+                    >
+                      Eliminar
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default AdminUser;
