9acd4203645f872606e83997fee9901753a6d50a
Rosemary <Rosemary>
2023-04-24 10:53:34 +0200

    add new change

--- __tests__/CardRecipe.test.jsx
+++ __tests__/CardRecipe.test.jsx
@@ -1,67 +1,67 @@
-// import { render, screen } from '@testing-library/react';
-// import CardRecipe from "../src/Component/CardRecipe.jsx";
-// import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import CardRecipe from "../src/Component/CardRecipe.jsx";
+import '@testing-library/jest-dom'
 
-// vi.mock('react-router-dom', () => ({
-//   useLoaderData: vi.fn(() => ({
-//     recipe: {
-//       title: 'recipe test',
-//       description: 'arroces',
-//       image: 'https://example.com/recipe.jpg',
-//       ingredients: ['Ingredient 1', 'Ingredient 2'],
-//       steps: ['Step 1', 'Step 2'],
-//       prepTime: '30 min',
-//       difficulty: 'medium',
-//       },
-//   })),
-// }));
+vi.mock('react-router-dom', () => ({
+  useLoaderData: vi.fn(() => ({
+    recipe: {
+      title: 'recipe test',
+      description: 'arroces',
+      image: 'https://example.com/recipe.jpg',
+      ingredients: ['Ingredient 1', 'Ingredient 2'],
+      steps: ['Step 1', 'Step 2'],
+      prepTime: '30 min',
+      difficulty: 'medium',
+      },
+  })),
+}));
 
-// describe("CardRecipe", () => {
-//     const recipe = {
-//       title: 'recipe test',
-//       description: 'arroces',
-//       image: 'https://example.com/recipe.jpg',
-//       ingredients: ['Ingredient 1', 'Ingredient 2'],
-//       steps: ['Step 1', 'Step 2'],
-//       prepTime: '30 min',
-//       difficulty: 'medium',
-//     };
+describe("CardRecipe", () => {
+    const recipe = {
+      title: 'recipe test',
+      description: 'arroces',
+      image: 'https://example.com/recipe.jpg',
+      ingredients: ['Ingredient 1', 'Ingredient 2'],
+      steps: ['Step 1', 'Step 2'],
+      prepTime: '30 min',
+      difficulty: 'medium',
+    };
   
-//     // beforeEach(() => {
-//     //   render(<CardRecipe title={recipe.title} description={recipe.description} image={recipe.image} />);
-//     // });
+    // beforeEach(() => {
+    //   render(<CardRecipe title={recipe.title} description={recipe.description} image={recipe.image} />);
+    // });
   
-//     test('should render recipe title', () => {
-//       const recipeTitle = screen.queryByText(/recipe test/i);
-//       expect(recipeTitle).toBeInTheDocument();
-//     });
+    test('should render recipe title', () => {
+      const recipeTitle = screen.queryByText(/recipe test/i);
+      expect(recipeTitle).toBeInTheDocument();
+    });
   
-//     test('should render recipe description', () => {
-//       const descriptionElement = screen.queryByText(/arroces/i);
-//       expect(descriptionElement).toBeInTheDocument();
-//     });
+    test('should render recipe description', () => {
+      const descriptionElement = screen.queryByText(/arroces/i);
+      expect(descriptionElement).toBeInTheDocument();
+    });
   
-//     test('renders recipe ingredients', () => {
-//       const ingredientsElement1 = screen.queryByText(/Ingredient 1/i);
-//       const ingredientsElement2 = screen.queryByText(/Ingredient 2/i);
-//       expect(ingredientsElement1).toBeInTheDocument();
-//       expect(ingredientsElement2).toBeInTheDocument();
-//     });
+    test('renders recipe ingredients', () => {
+      const ingredientsElement1 = screen.queryByText(/Ingredient 1/i);
+      const ingredientsElement2 = screen.queryByText(/Ingredient 2/i);
+      expect(ingredientsElement1).toBeInTheDocument();
+      expect(ingredientsElement2).toBeInTheDocument();
+    });
   
-//     test('should render recipe image', () => {
-//       const imageElement = screen.getByRole('img');
-//       expect(imageElement).toBeInTheDocument();
-//       expect(imageElement).toHaveAttribute('src', recipe.image);
-//     });
+    test('should render recipe image', () => {
+      const imageElement = screen.getByRole('img');
+      expect(imageElement).toBeInTheDocument();
+      expect(imageElement).toHaveAttribute('src', recipe.image);
+    });
   
-//     test('should render prep time', () => {
-//       const prepTimeElement = screen.queryByText(/30 min/i);
-//       expect(prepTimeElement).toBeInTheDocument();
-//     });
+    test('should render prep time', () => {
+      const prepTimeElement = screen.queryByText(/30 min/i);
+      expect(prepTimeElement).toBeInTheDocument();
+    });
   
-//     test('should render difficulty', () => {
-//       const difficultyElement = screen.queryByText(/medium/i);
-//       expect(difficultyElement).toBeInTheDocument();
-//     });
-//   });
+    test('should render difficulty', () => {
+      const difficultyElement = screen.queryByText(/medium/i);
+      expect(difficultyElement).toBeInTheDocument();
+    });
+  });
   
--- src/Component/CardRecipe.jsx
+++ src/Component/CardRecipe.jsx
@@ -1,38 +1,38 @@
-// import React from 'react';
-// import { Card, CardContent, CardMedia, Typography } from '@mui/material';
-// import CardRecipe from "../Component/CardRecipe.jsx"
+import React from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import CardRecipe from "../Component/CardRecipe.jsx"
 
-// const CardRecipe = ({ title, description, ingredients, difficulty, preptime, image }) => {
-//   return (
-//     <Card>
-//       <CardMedia
-//         component="img"
-//         height="140"
-//         image={image}
-//         alt="recipe"
-//       />
-//       <CardContent>
-//         <Typography gutterBottom variant="h5" component="div">
-//           {title}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {description}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {ingredients}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {preptime}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {difficulty}
-//         </Typography>
-//       </CardContent>
-//     </Card>
-//   );
-// };
+const CardRecipe = ({ title, description, ingredients, difficulty, preptime, image }) => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="140"
+        image={image}
+        alt="recipe"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {ingredients}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {preptime}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {difficulty}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
-// export default CardRecipe;
+export default CardRecipe;
 
 
 
