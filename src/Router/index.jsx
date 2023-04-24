c3187daf3d16bfb803445605cf8634cdbfae5bf5
Anyi79 <ajfr79@gmail.com>
2023-04-24 09:43:22 +0200

    add fetch fix

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -76,7 +76,7 @@ export const router = createBrowserRouter([
                     },
                     {   path: '/AdminList',
                         element: <AdminList/>,
-                        loadre: fetchOrders,
+                        loader: fetchOrders,
                     }
                     
                 ]
