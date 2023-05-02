d3aecf5a4f7b99e45c045dcafe8c78403dfa1cd4
Rosemary <Rosemary>
2023-05-02 11:13:31 +0200

    add file

---
+++ __tests__/Register.test.jsx
@@ -0,0 +1,48 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Register from '../src/Component/Register';
+
+describe('Register', () => {
+  test('should render all form fields', () => {
+    render(<Register />);
+    const userNameField = screen.getByLabelText(/username/i);
+    const passwordField = screen.getByLabelText(/password/i);
+    const confirmPasswordField = screen.getByLabelText(/confirm password/i);
+    const userEmailField = screen.getByLabelText(/email/i);
+    const userPhoneField = screen.getByLabelText(/phone/i);
+
+    expect(userNameField).toBeInTheDocument();
+    expect(passwordField).toBeInTheDocument();
+    expect(confirmPasswordField).toBeInTheDocument();
+    expect(userEmailField).toBeInTheDocument();
+    expect(userPhoneField).toBeInTheDocument();
+  });
+
+  test('should update form field values on user input', () => {
+    render(<Register />);
+    const userNameField = screen.getByLabelText(/username/i);
+    const passwordField = screen.getByLabelText(/password/i);
+    const confirmPasswordField = screen.getByLabelText(/confirm password/i);
+    const userEmailField = screen.getByLabelText(/email/i);
+    const userPhoneField = screen.getByLabelText(/phone/i);
+
+    const inputValues = {
+      userName: 'testUser',
+      password: 'testPassword',
+      confirmPassword: 'testPassword',
+      userEmail: 'testUser@example.com',
+      userPhone: '1234567890',
+    };
+
+    fireEvent.change(userNameField, { target: { value: inputValues.userName } });
+    fireEvent.change(passwordField, { target: { value: inputValues.password } });
+    fireEvent.change(confirmPasswordField, { target: { value: inputValues.confirmPassword } });
+    fireEvent.change(userEmailField, { target: { value: inputValues.userEmail } });
+    fireEvent.change(userPhoneField, { target: { value: inputValues.userPhone } });
+
+    expect(userNameField).toHaveValue(inputValues.userName);
+    expect(passwordField).toHaveValue(inputValues.password);
+    expect(confirmPasswordField).toHaveValue(inputValues.confirmPassword);
+    expect(userEmailField).toHaveValue(inputValues.userEmail);
+    expect(userPhoneField).toHaveValue(inputValues.userPhone);
+  });
+});
