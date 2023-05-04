f87a1cb43c3aa85329954789c755f6a05126fb7d
acalabustos <acalabustos@gmail.com>
2023-05-04 21:28:51 +0200

    change table order

--- src/Pages/AdminList.jsx
+++ src/Pages/AdminList.jsx
@@ -1,61 +1,53 @@
 import React from 'react';
-import {  useLoaderData } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
+import { useLoaderData } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Style/AdminList.css'
+import '../Style/AdminList.css';
+import Table from 'react-bootstrap/Table';
 
 export default function ListAdmin() {
-  const {orders} = useLoaderData();
-  console.log  (orders)
+  const { orders } = useLoaderData();
+
   const handleSendEmail = () => {
     window.location.href = 'mailto:cooksmart56@gmail.com';
   };
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-    <div className="row">
-      <div>
-      {orders.map((order) => (
-        <div key={order.id} className="col col-4">
-            El profe ha pedido esto:
-            {order.map(orderItem => {
-              return (
-                <Card style={{ width: '13rem' }} className="m-4">
-             
-                <Card.Body>
-                  <Card.Text>
-                    Uduario
-                    {orderItem.idUser}
-                    {orderItem.UserName}
-                  </Card.Text>
-                  <Card.Text>
-                    Ingrediente
-                    {orderItem.idIngredient}
-                    {orderItem.IngredientName}
-                  </Card.Text>
-                  <Card.Text>
-                    Cantidad
-                    {orderItem.amount}
-                  </Card.Text>
-                  <Card.Text>
-                    Unidad
-                    {orderItem.unit}
-                  </Card.Text>
-                </Card.Body>
-                
-              </Card>
-              )
-            })}
-            
-          
+      <div className="row">
+        <div>
+          <h3>Listado de pedidos</h3>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Usuario</th>
+                <th>Ingrediente</th>
+                <th>Cantidad</th>
+                <th>Unidad</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) =>
+                order.map((orderItem) => (
+                  <tr key={orderItem.id}>
+                    <td>{orderItem.username}</td>
+                    <td>{orderItem.ingredientName}</td>
+                    <td>{orderItem.amount}</td>
+                    <td>{orderItem.unit}</td>
+                    <td>{orderItem.insertDate}</td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </Table>
         </div>
-      ))}
-      </div>
-     <div className="col col-12 d-flex justify-content-center">
-     <button className="btnSendEmail" onClick={handleSendEmail}>Enviar correo electrónico</button>
+        
+        <div className="col col-12 d-flex justify-content-center">
+          <button className="btnSendEmail" onClick={handleSendEmail}>
+            Enviar correo electrónico
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
