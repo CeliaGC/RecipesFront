49d67bdebe8763991f509adf4b9f94ab68095b75
Anyi79 <ajfr79@gmail.com>
2023-05-05 02:11:04 +0200

    add new change

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -10,8 +10,8 @@ const NavbarLog = () => {
     <Navbar expand="lg" variant="light" style={{ marginBottom: '-20px', backgroundColor: "#F2AA85" }} >
     <Container className="d-flex flex-column flex-md-row">
         <Nav className="w-100 justify-content-around flex-row">
-            <Nav.Link href='/' style={{ color: "black", marginBottom: "1rem" }}>CookSmart</Nav.Link>
-            <Nav.Link href='/Loginview' style={{ color: "black", marginBottom: "1rem", marginLeft: '20px' }}>
+            <Nav.Link href='/' style={{ color: "black", marginBottom: "1rem", fontSize:'35px' }}>CookSmart</Nav.Link>
+            <Nav.Link href='/Loginview' style={{ color: "black", marginBottom: "1rem", marginLeft: '20px', fontSize:'20px' }}>
                 Iniciar sesión</Nav.Link>
             <Nav.Link href="https://www.ucam.edu/estudios/grados/gastronomia-presencial" style={{ color: "black", marginBottom: "1rem", fontSize:'20px' }}>Acceso a blog</Nav.Link>
         </Nav>
