330a65c3bdc3c24416b7c94e9c8d28458c038011
acalabustos <acalabustos@gmail.com>
2023-05-05 08:50:46 +0200

    finish change

--- src/Pages/MenuRecipe.jsx
+++ src/Pages/MenuRecipe.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import CardRecipe from "../Component/CardRecipe";
 import NavbarViewTeacher from "../Component/NavbarViewTeacher";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import C7A from "../assets/C3.jpg";
+
 
 
 function MenuRecipe() {
   return (
-    <div >
-    <NavbarViewTeacher></NavbarViewTeacher>
-    <CardRecipe/>
-
-    
-       
+    <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-3">
+        <NavbarViewTeacher />
+      </div>
+      <div className="col-md-9">
+        <CardRecipe />
+      </div>
     </div>
+  </div>
   )
 }
 
